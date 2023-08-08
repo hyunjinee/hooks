36dd45afbd05732a6a73b4147f818d276793d2f8
hyunjin <leehj0110@kakao.com>
2023-08-08 14:22:56 +0900

    docs: useIsomorphicLayoutEffect

---
+++ .changeset/healthy-snakes-pretend.md
@@ -0,0 +1,5 @@
+---
+'@hyunjin/hooks': patch
+---
+
+useOnClickOutside
--- src/useIsomorphicLayoutEffect/useIsomorphicLayoutEffect.ts
+++ src/useIsomorphicLayoutEffect/useIsomorphicLayoutEffect.ts
@@ -1,7 +1,18 @@
 import { useEffect, useLayoutEffect } from 'react'
 
 /**
- * Match Logic w/ React Source code. shared/ExecutionEnvironment
+ *  Description from React-Redux Source Code
+ *
+ *  Match Logic w/ React Source code. shared/ExecutionEnvironment
+ *
+ *  React currently throws a warning when using useLayoutEffect on the server.
+ *  To get around it, we can conditionally useEffect on the server (no-op) and
+ *  useLayoutEffect in the browser. We need useLayoutEffect to ensure the store
+ *  subscription callback always has the selector from the latest render commit
+ *  available, otherwise a store update may happen between render and the effect,
+ *  which may cause missed updates; we also must ensure the store subscription
+ *  is created synchronously, otherwise a store update may occur before the
+ *  subscription is created and an inconsistent state may be observed
  */
 export const canUseDOM = !!(
   typeof window !== 'undefined' &&
@@ -10,14 +21,3 @@ export const canUseDOM = !!(
 )
 
 export const useIsomorphicLayoutEffect = canUseDOM ? useLayoutEffect : useEffect
-
-// Desciprtion from React-Redux Source Code
-
-// React currently throws a warning when using useLayoutEffect on the server.
-// To get around it, we can conditionally useEffect on the server (no-op) and
-// useLayoutEffect in the browser. We need useLayoutEffect to ensure the store
-// subscription callback always has the selector from the latest render commit
-// available, otherwise a store update may happen between render and the effect,
-// which may cause missed updates; we also must ensure the store subscription
-// is created synchronously, otherwise a store update may occur before the
-// subscription is created and an inconsistent state may be observed
