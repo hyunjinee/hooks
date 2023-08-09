425647124f5bad88ed349f0994a3e21b236d7bb2
hyunjin <leehj0110@kakao.com>
2023-08-09 13:59:27 +0900

    install jsdoc

--- src/useToggle/useToggle.ts
+++ src/useToggle/useToggle.ts
@@ -1,8 +1,18 @@
 import { useCallback, useState } from 'react'
 
-export const useToggle = (defaultValue?: boolean) => {
+/**
+ *
+ * @param {T | undefined} [defaultValue] toggle하고 싶은 초기값입니다.
+ *
+ * @returns {[T, () => void, typeof setValue]} [value, toggle, setValue]를 반환합니다.
+ *
+ * @description toggle을 위한 훅입니다.
+ */
+export const useToggle = <T>(
+  defaultValue?: T,
+): [boolean, () => void, typeof setValue] => {
   const [value, setValue] = useState(!!defaultValue)
   const toggle = useCallback(() => setValue((prev) => !prev), [])
 
-  return [value, toggle, setValue] as [boolean, () => void, typeof setValue]
+  return [value, toggle, setValue]
 }
