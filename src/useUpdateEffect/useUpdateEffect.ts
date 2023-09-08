849345f2e99081968e65a6edd8ba98ca2dfaa588
hyunjin <leehj0110@kakao.com>
2023-09-08 16:37:07 +0900

    feat: useUpdateEffect

---
+++ src/useUpdateEffect/useUpdateEffect.ts
@@ -0,0 +1,18 @@
+import { type EffectCallback, type DependencyList } from 'react'
+
+import { useEffect } from 'react'
+import { useIsFirstRender } from '../useIsFirstRender/useIsFirstRender'
+
+export const useUpdateEffect = (
+  effect: EffectCallback,
+  deps: DependencyList,
+) => {
+  const isFirst = useIsFirstRender()
+
+  useEffect(() => {
+    if (!isFirst) {
+      return effect()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps)
+}
