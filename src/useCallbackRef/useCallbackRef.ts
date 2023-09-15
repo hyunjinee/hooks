cdd5a6a1a63c7abee10fc3491164cf5215e243c9
hyunjin <leehj0110@kakao.com>
2023-09-15 20:02:27 +0900

    feat: useCallbackRef

---
+++ src/useCallbackRef/useCallbackRef.ts
@@ -0,0 +1,13 @@
+import { useRef, useEffect, useMemo } from 'react'
+
+export const useCallbackRef = <T extends (...args: unknown[]) => unknown>(
+  callback: T | undefined,
+): T => {
+  const callbackRef = useRef(callback)
+
+  useEffect(() => {
+    callbackRef.current = callback
+  })
+
+  return useMemo(() => ((...args) => callbackRef.current?.(...args)) as T, [])
+}
